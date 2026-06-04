c2b00bd465db7e50fc8aae188cc8955bf3389267
adarshyoho-ops <adarshyoho@gmail.com>
2026-06-04 10:50:02 +0530

    Bind application to all network interfaces

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
 
-  await app.listen(port);
+  await app.listen(port,'0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
